243d4decf27435fcf860f830e72a5539d3836fb0
Matthew Evers <34583379+MRobertEvers@users.noreply.github.com>
2025-05-09 12:09:08 -0500

    more cleanup

--- src/projection.c
+++ src/projection.c
@@ -12,9 +12,9 @@ extern int g_tan_table[2048];
  */
 struct ProjectedTriangle
 project(
-    int x1,
-    int y1,
-    int z1,
+    int x,
+    int y,
+    int z,
     int yaw,
     int pitch,
     int roll,
@@ -37,9 +37,9 @@ project(
     int sin_camera_roll = g_sin_table[camera_roll];
 
     // Field of view parameters
-    // Conceptually, Z_FOCAL is the focal distance of the camera, points on the near plane
-    // are projected to the screen with a scale factor of Z_FOCAL/z1_final = fov_scale
-    const int Z_FOCAL = 512;
+    // Conceptually, Z_NEAR is the location of the screen. At this point, points on the near plane
+    // are projected to the screen with a scale factor of Z_FOCAL/z_final = fov_scale
+    const int Z_NEAR = 512;
     // const int Z_FAR = 1500;
     // const int ASPECT_RATIO = 1;
 
@@ -52,49 +52,50 @@ project(
     int cos_roll = g_cos_table[roll];
 
     // Rotate around Y-axis (yaw)
-    int x1_rotated = x1 * cos_yaw - z1 * sin_yaw;
-    x1_rotated >>= 16;
-    int z1_rotated = x1 * sin_yaw + z1 * cos_yaw;
-    z1_rotated >>= 16;
+    int x_rotated = x * cos_yaw - z * sin_yaw;
+    x_rotated >>= 16;
+    int z_rotated = x * sin_yaw + z * cos_yaw;
+    z_rotated >>= 16;
 
     // Rotate around X-axis (pitch)
-    int y1_rotated = y1 * cos_pitch - z1_rotated * sin_pitch;
-    y1_rotated >>= 16;
-    int z1_rotated2 = y1 * sin_pitch + z1_rotated * cos_pitch;
-    z1_rotated2 >>= 16;
+    int y_rotated = y * cos_pitch - z_rotated * sin_pitch;
+    y_rotated >>= 16;
+    int z_rotated2 = y * sin_pitch + z_rotated * cos_pitch;
+    z_rotated2 >>= 16;
 
     // Rotate around Z-axis (roll)
-    int x1_final = x1_rotated * cos_roll - y1_rotated * sin_roll;
-    x1_final >>= 16;
-    int y1_final = x1_rotated * sin_roll + y1_rotated * cos_roll;
-    y1_final >>= 16;
+    int x_final = x_rotated * cos_roll - y_rotated * sin_roll;
+    x_final >>= 16;
+    int y_final = x_rotated * sin_roll + y_rotated * cos_roll;
+    y_final >>= 16;
 
     // Translate points relative to camera position
-    x1_final += scene_x;
-    y1_final += scene_y;
-    z1_rotated2 += scene_z;
+    x_final += scene_x;
+    y_final += scene_y;
+    z_rotated2 += scene_z;
 
-    // Apply scene rotation
+    // Apply perspective rotation
     // First rotate around Y-axis (scene yaw)
-    int x1_scene = x1_final * cos_camera_yaw - z1_rotated2 * sin_camera_yaw;
-    x1_scene >>= 16;
-    int z1_scene = x1_final * sin_camera_yaw + z1_rotated2 * cos_camera_yaw;
-    z1_scene >>= 16;
+    int x_scene = x_final * cos_camera_yaw - z_rotated2 * sin_camera_yaw;
+    x_scene >>= 16;
+    int z_scene = x_final * sin_camera_yaw + z_rotated2 * cos_camera_yaw;
+    z_scene >>= 16;
 
     // Then rotate around X-axis (scene pitch)
-    int y1_scene = y1_final * cos_camera_pitch - z1_scene * sin_camera_pitch;
-    y1_scene >>= 16;
-    int z1_scene2 = y1_final * sin_camera_pitch + z1_scene * cos_camera_pitch;
-    z1_scene2 >>= 16;
+    int y_scene = y_final * cos_camera_pitch - z_scene * sin_camera_pitch;
+    y_scene >>= 16;
+    int z_scene2 = y_final * sin_camera_pitch + z_scene * cos_camera_pitch;
+    z_scene2 >>= 16;
 
     // Finally rotate around Z-axis (scene roll)
-    int x1_final_scene = x1_scene * cos_camera_roll - y1_scene * sin_camera_roll;
-    x1_final_scene >>= 16;
-    int y1_final_scene = x1_scene * sin_camera_roll + y1_scene * cos_camera_roll;
-    y1_final_scene >>= 16;
+    int x_final_scene = x_scene * cos_camera_roll - y_scene * sin_camera_roll;
+    x_final_scene >>= 16;
+    int y_final_scene = x_scene * sin_camera_roll + y_scene * cos_camera_roll;
+    y_final_scene >>= 16;
+
+    int z_final = z_scene2;
 
     // Perspective projection with FOV
-    int z1_final = z1_scene2;
 
     // Calculate FOV scale based on the angle using sin/cos tables
     // fov is in units of (2π/2048) radians
@@ -109,22 +110,22 @@ project(
     int cot_fov_half = g_tan_table[1536 - fov_half];
 
     // Apply FOV scaling to x and y coordinates
-    int fov_scale_ish16 = (cot_fov_half * Z_FOCAL) / z1_final;
+    int fov_scale_ish16 = (cot_fov_half * Z_NEAR) / z_final;
 
     // Project to screen space with FOV
-    int screen_x1 = ((x1_final_scene * fov_scale_ish16) >> 16) + screen_width / 2;
-    int screen_y1 = ((y1_final_scene * fov_scale_ish16) >> 16) + screen_height / 2;
+    int screen_x = ((x_final_scene * fov_scale_ish16) >> 16) + screen_width / 2;
+    int screen_y = ((y_final_scene * fov_scale_ish16) >> 16) + screen_height / 2;
 
     // z is the distance from the camera.
     // It is a judgement call to say when to cull the triangle, but
     // things you can consider are the average size of models.
     // It is up to the caller to cull the triangle if it is too close or behind the camera.
-    // e.g. z1_final <= 50
+    // e.g. z_final <= 50
 
     // Set the projected triangle
-    projected_triangle.x1 = screen_x1;
-    projected_triangle.y1 = screen_y1;
-    projected_triangle.z1 = z1_final;
+    projected_triangle.x1 = screen_x;
+    projected_triangle.y1 = screen_y;
+    projected_triangle.z1 = z_final;
 
     return projected_triangle;
 }
