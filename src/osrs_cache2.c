c07eec46184d47fd04ab0cc99846aef349a77f95
Matthew Evers <34583379+MRobertEvers@users.noreply.github.com>
2025-05-16 11:16:05 -0500

    reading master index correctly

--- src/osrs_cache2.c
+++ src/osrs_cache2.c
@@ -3,6 +3,10 @@
 #include <bzlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CACHE_PATH "../cache"
 
 // public byte[] data;
 // public int fileCount;
@@ -26,6 +30,7 @@ struct Buffer
 {
     char* data;
     int position;
+    int data_size;
 };
 
 static int
@@ -49,6 +54,21 @@ read_32(char* data, int data_size, struct Buffer* buffer)
     buffer->position += 4;
     return (data[buffer->position - 4] << 24) | (data[buffer->position - 3] << 16) |
            (data[buffer->position - 2] << 8) | data[buffer->position - 1];
+}
+
+static int
+readto(char* out, int out_size, int len, struct Buffer* buffer)
+{
+    int bytes_read = 0;
+    while( len > 0 && buffer->position < buffer->data_size )
+    {
+        out[bytes_read] = buffer->data[buffer->position];
+        len--;
+        buffer->position++;
+        bytes_read++;
+    }
+
+    return bytes_read;
 }
 
 static void
@@ -148,6 +168,7 @@ parse_file_archive(char* data, int data_size, struct FileArchive* file_archive)
 }
 
 #define INDEX_255_ENTRY_SIZE 6
+#define SECTOR_SIZE 520
 
 // Index255 is sometimes known as ref table.
 
@@ -158,11 +179,158 @@ load_index255(char* data, int data_size)
     int index_count = data_size / INDEX_255_ENTRY_SIZE;
 }
 
+/**
+ * @brief "idx" files are indexes.
+ */
 struct IndexRecord
 {
-    int file_id;
-    int id, sector, length;
+    int idx_file_id; // The file extension, e.g. 2 := idx2
+    int record_id, sector, length;
 };
+
+static void
+read_index_entry(
+    int index_file_id, char* data, int data_size, int entry_slot, struct IndexRecord* record)
+{
+    struct Buffer buffer = { .data = data, .position = entry_slot * INDEX_255_ENTRY_SIZE };
+
+    int length = (data[buffer.position] << 16) | (data[buffer.position + 1] << 8) |
+                 (data[buffer.position + 2] & 0xFF);
+    int sector = (data[buffer.position + 3] << 16) | (data[buffer.position + 4] << 8) |
+                 (data[buffer.position + 5] & 0xFF);
+
+    if( length <= 0 || sector <= 0 )
+    {
+        return;
+    }
+
+    record->length = length;
+    record->sector = sector;
+    record->record_id = entry_slot;
+    record->idx_file_id = index_file_id;
+}
+
+/**
+ * @brief In the dat2 file, the records are archives.
+ *
+ * @param data
+ * @param data_size
+ * @param idx_file_id The idx file that contains the record. I.e. The block of data knows what index
+ * file indexes it. This should be the idx file id of the index file.
+ * @param record_id
+ * @param sector
+ * @param length
+ */
+static char*
+read_dat2(char* data, int data_size, int idx_file_id, int record_id, int sector, int length)
+{
+    struct Buffer data_buffer = { .data = data, .position = 0, .data_size = data_size };
+
+    char read_buffer[SECTOR_SIZE];
+    int read_buffer_len = 0;
+
+    int header_size;
+    int data_block_size;
+    int current_archive;
+    int current_part;
+    int next_sector;
+    int current_index = 0;
+
+    if( sector <= 0L || data_size / SECTOR_SIZE < (long)sector )
+    {
+        printf("bad read, dat length %d, requested sector %d", data_size, sector);
+        return NULL;
+    }
+
+    int out_len = 0;
+    char* out = malloc(length);
+    memset(out, 0, length);
+
+    for( int part = 0, read_bytes_count = 0; length > read_bytes_count; sector = next_sector )
+    {
+        if( sector == 0 )
+        {
+            printf("Unexpected end of file\n");
+            return NULL;
+        }
+        data_buffer.position = sector * SECTOR_SIZE;
+
+        data_block_size = length - read_bytes_count;
+        if( record_id > 0xFFFF )
+        {
+            header_size = 10;
+            if( data_block_size > SECTOR_SIZE - header_size )
+                data_block_size = SECTOR_SIZE - header_size;
+
+            int bytes_read = readto(
+                read_buffer, sizeof(read_buffer), header_size + data_block_size, &data_buffer);
+            if( bytes_read < header_size + data_block_size )
+            {
+                printf("short read when reading file data for %d/%d\n", record_id, current_index);
+                return NULL;
+            }
+
+            read_buffer_len = bytes_read;
+
+            current_archive = ((read_buffer[0] & 0xFF) << 24) | ((read_buffer[1] & 0xFF) << 16) |
+                              ((read_buffer[2] & 0xFF) << 8) | (read_buffer[3] & 0xFF);
+            current_part = ((read_buffer[4] & 0xFF) << 8) + (read_buffer[5] & 0xFF);
+            next_sector = ((read_buffer[6] & 0xFF) << 16) | ((read_buffer[7] & 0xFF) << 8) |
+                          (read_buffer[8] & 0xFF);
+            current_index = read_buffer[9] & 0xFF;
+        }
+        else
+        {
+            header_size = 8;
+            if( data_block_size > SECTOR_SIZE - header_size )
+                data_block_size = SECTOR_SIZE - header_size;
+
+            int bytes_read = readto(
+                read_buffer, sizeof(read_buffer), header_size + data_block_size, &data_buffer);
+            if( bytes_read < header_size + data_block_size )
+            {
+                printf("short read when reading file data for %d/%d\n", record_id, current_index);
+                return NULL;
+            }
+
+            read_buffer_len = bytes_read;
+
+            current_archive = ((read_buffer[0] & 0xFF) << 8) | (read_buffer[1] & 0xFF);
+            current_part = ((read_buffer[2] & 0xFF) << 8) | (read_buffer[3] & 0xFF);
+            next_sector = ((read_buffer[4] & 0xFF) << 16) | ((read_buffer[5] & 0xFF) << 8) |
+                          (read_buffer[6] & 0xFF);
+            current_index = read_buffer[7] & 0xFF;
+        }
+
+        if( record_id != current_archive || current_part != part || idx_file_id != current_index )
+        {
+            printf(
+                "data mismatch %d != %d, %d != %d, %d != %d\n",
+                record_id,
+                current_archive,
+                part,
+                current_part,
+                idx_file_id,
+                current_index);
+            return NULL;
+        }
+
+        if( next_sector < 0 || data_size / SECTOR_SIZE < (long)next_sector )
+        {
+            printf("invalid next sector");
+            return NULL;
+        }
+
+        memcpy(out + out_len, read_buffer + header_size, data_block_size);
+        out_len += data_block_size;
+
+        read_bytes_count += data_block_size;
+
+        ++part;
+    }
+
+    return out;
+}
 
 /**
  * Load .dat2 file
@@ -183,4 +351,47 @@ struct IndexRecord
  */
 struct Model*
 load_models()
-{}
+{
+    char* dat2_data;
+    int dat2_size;
+    FILE* dat2 = fopen(CACHE_PATH "/main_file_cache.dat2", "rb");
+    if( dat2 == NULL )
+    {
+        printf("Failed to open dat2 file");
+        return NULL;
+    }
+    fseek(dat2, 0, SEEK_END);
+    dat2_size = ftell(dat2);
+    fseek(dat2, 0, SEEK_SET);
+    dat2_data = malloc(dat2_size);
+    fread(dat2_data, 1, dat2_size, dat2);
+    fclose(dat2);
+
+    int master_index_size;
+    char* master_index_data;
+    FILE* master_index = fopen(CACHE_PATH "/main_file_cache.idx255", "rb");
+    if( master_index == NULL )
+    {
+        printf("Failed to open master index file");
+        return NULL;
+    }
+    fseek(master_index, 0, SEEK_END);
+    master_index_size = ftell(master_index);
+    fseek(master_index, 0, SEEK_SET);
+    master_index_data = malloc(master_index_size);
+    fread(master_index_data, 1, master_index_size, master_index);
+    fclose(master_index);
+
+    int master_index_record_count = master_index_size / INDEX_255_ENTRY_SIZE;
+
+    for( int i = 0; i < master_index_record_count; i++ )
+    {
+        struct IndexRecord record;
+        read_index_entry(255, master_index_data, master_index_size, i, &record);
+
+        // record now contains a region in the .dat2 file that contains the data indexed by
+        // idx<i>.
+
+        read_dat2(dat2_data, dat2_size, 255, record.record_id, record.sector, record.length);
+    }
+}
