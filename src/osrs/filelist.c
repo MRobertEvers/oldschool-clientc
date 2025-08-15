2d54db09c6e189ad789ae33acf333c66eb6deb79
Matthew Evers <34583379+MRobertEvers@users.noreply.github.com>
2025-08-15 07:36:29 -0500

    remove buffer from filelist

--- src/osrs/filelist.c
+++ src/osrs/filelist.c
@@ -1,6 +1,6 @@
 #include "osrs/filelist.h"
 
-#include "buffer.h"
+#include "rsbuf.h"
 
 #include <assert.h>
 #include <stdio.h>
@@ -24,7 +24,7 @@ filelist_new_from_decode(char* data, int data_size, int num_files)
     if( !filelist )
         return NULL;
 
-    struct Buffer buffer = { .data = data, .position = 0, .data_size = data_size };
+    struct RSBuffer buffer = { .data = data, .position = 0, .size = data_size };
 
     filelist->files = malloc(num_files * sizeof(char*));
     filelist->file_sizes = malloc(num_files * sizeof(int));
@@ -49,8 +49,8 @@ filelist_new_from_decode(char* data, int data_size, int num_files)
     }
 
     /* read the number of chunks at the end of the archive */
-    buffer.position = buffer.data_size - 1;
-    int chunks = read_8(&buffer);
+    buffer.position = buffer.size - 1;
+    int chunks = g1(&buffer);
     buffer.position = 0;
 
     /* read the sizes of the child entries and individual chunks */
@@ -73,14 +73,14 @@ filelist_new_from_decode(char* data, int data_size, int num_files)
         goto error;
     memset(sizes, 0, num_files * sizeof(int));
 
-    buffer.position = buffer.data_size - 1 - chunks * num_files * 4;
+    buffer.position = buffer.size - 1 - chunks * num_files * 4;
     for( int chunk = 0; chunk < chunks; chunk++ )
     {
         int chunk_size = 0;
         for( int id = 0; id < num_files; id++ )
         {
             /* read the delta-encoded chunk length */
-            int delta = read_32(&buffer);
+            int delta = g4(&buffer);
             chunk_size += delta;
 
             /* store the size of this chunk */
@@ -112,11 +112,11 @@ filelist_new_from_decode(char* data, int data_size, int num_files)
             int chunk_size = chunk_sizes[chunk][id];
 
             /* copy this chunk into the file buffer */
-            readto(
+            greadto(
+                &buffer,
                 filelist->files[id] + file_offsets[id],
                 sizes[id] - file_offsets[id],
-                chunk_size,
-                &buffer);
+                chunk_size);
             file_offsets[id] += chunk_size;
             filelist->file_sizes[id] += chunk_size;
         }
